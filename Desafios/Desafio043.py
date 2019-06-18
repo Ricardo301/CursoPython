9afcad621983f611293c6df17caa3efc4f1f51a4
Ricardo301 <33549097+Ricardo301@users.noreply.github.com>
2019-06-18 14:55:23 -0300

    18/06

---
+++ Desafios/Desafio043.py
@@ -0,0 +1,15 @@
+peso = float(input('Digite seu peso: '))
+altura = float(input('Digite sua altura: '))
+imc = peso/ (altura*altura)
+print('Seu imc é de : {}'.format(imc))
+
+if imc < 18.5:
+    print('Abaixo do Peso')
+elif 18.5 <= imc < 25:
+    print('Peso ideal')
+elif 25 <= imc < 30:
+    print('sobre peso')
+elif 30 <= imc < 40:
+    print('Obseso')
+else:
+    print('Obesidade morbida')
