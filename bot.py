bfdcbc16182d2e2d5066602cc195561d27896ed9
Ricardo301 <33549097+Ricardo301@users.noreply.github.com>
2019-07-15 22:41:58 -0300

    Bot

--- bot.py
+++ bot.py
@@ -3,21 +3,17 @@ import discord
 TOKEN = 'NDY3ODEwNzUwNjg2Mjk4MTEy.XS0hrg.B9JJrIeUxSM3UFf1SBz-7F44CKM'
 client = discord.Client()
 
-@client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
 
 @client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+    print('We have logged in as {0.user}'.format(client))
 
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Iae irmão!')
 client.run(TOKEN)
